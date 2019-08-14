32c1cad2c4c49dada4a69bed9841f33cbc9ef1e6
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-08-14 07:57:30 +0530

    server setup and synchronization with database model was  done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app')
+const db = require('./models/index')
 const port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log(`Application running on port ${port}.`)
+    db.sequelize.sync();
 })
