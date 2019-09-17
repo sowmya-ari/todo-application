773891faedf9279f384f6e0500e655af40f11c3f
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-09-17 11:40:24 +0530

    moved routes to separate folder

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const cors= require('cors')
-const controller = require('./controllers/todo')
+const routes = require('./routes/todoRouter')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,12 +12,6 @@ app.use(
   })
 )
 
-app.get('/allTasks',controller.getAllTasks)
-app.get('/task/active',controller.getActiveTasks)
-app.get('/task/completed',controller.getCompletedTasks)
-app.post('/newTask',controller.postNewTask)
-app.delete('/task/:id',controller.deleteTask)
-app.put('/task/:id',controller.editTask)
-app.patch('/task/:id',controller.markTaskAsCompleted)
+app.use(routes)
 
 module.exports = app
