7a8e542306d8c368900169abeee7c702ebebde96
Sowmya Ari <sowmya@Sowmyas-Pro.local>
2019-10-15 14:11:02 +0530

    test cases are modified according to sequelize-mocking

---
+++ server/test/todo.test.js
@@ -0,0 +1,64 @@
+var SequelizeMock = require("sequelize-mock")
+var dbMock = new SequelizeMock()
+var chai = require('chai')
+var expect = chai.expect;
+var task = dbMock.define('Todo');
+task.$queueResult([
+    task.build({
+      'task': 'Jenkins',
+      'status': 'active'
+    }),
+    task.build({
+      'task': 'unit testing',
+      'status': 'completed'
+    })
+  ]);
+describe("Test Sequelize Mocking", () => {  
+    it("Should get all the values from mock model", async () => {
+        const todo = await task.findAll({
+            where: {status:"active"}
+        });
+        expect(todo.length).to.equal(2);
+    })
+    it("Should create value into the model", async () => {
+        const todo = await task.create({
+            task: "hello",
+            status: "completed"
+        });
+        console.log(todo);
+        expect(todo.get('task')).to.equal('hello');
+        expect(todo.get('status')).to.equal('completed');
+    })
+    it("Should create value into the model", async () => {
+      const todo = await task.create({
+          task: "hello",
+          status: "completed"
+      });
+      console.log(todo);
+      expect(todo.get('task')).to.equal('hello');
+      expect(todo.get('status')).to.equal('completed');
+    })
+   it("Should delete value from the model", async () => {
+     const todo = await task.destroy({ 
+       where: {
+        id: 3
+       }
+      });
+     console.log(todo);
+     expect(todo).to.equal(1);
+    })
+    it("Should update status from the model", async () => {
+      const todo = await task.update( 
+        {
+          status: "completed" 
+        }, 
+        {where: {
+         id: 1
+        }
+      });
+      console.log(todo)
+      expect(todo).to.be.an('array').that.includes(1);
+    })
+})
+    
+  
