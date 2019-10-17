c6c0cf510ab9924c2433253ba7c90359175dc827
Sowmya Ari <sowmya@Sowmyas-Pro.Dlink>
2019-10-17 12:38:30 +0530

    some unused ansible files are deleted

--- todo-ansible/seeders.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO testtables (username)
-VALUES
-   ('sowmya'),('syamala');
--- todo-ansible/table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE testtables(
-   user_id serial PRIMARY KEY,
-   username VARCHAR (50) UNIQUE NOT NULL
-);
